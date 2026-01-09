3ed420ac8cda15def72405141ac4cfb4f3d90d97
Mikołaj Garstecki <plgmikolajg@login01.helios.cyfronet.pl>
2026-01-09 13:54:32 +0100

    mrunner fix

--- mrunner_run.py
+++ mrunner_run.py
@@ -8,20 +8,24 @@ if __name__ == "__main__":
     entry_point = cfg.pop("entry_point", "train")
     cfg.pop("experiment_id")
 
-    key_pairs = []
+    
+    cmd = ["python", "-m", entry_point]
     for key, value in cfg.items():
-        if isinstance(value, bool):
-            key_list = key.split(".")
-            key_list[-1] = key_list[-1] if value else f"no-{key_list[-1]}"
-            key = ".".join(key_list)
-            key_pairs.append(f"--{key}")
-        elif isinstance(value, list):
-            key_pairs.append(f"--{key}")
-            for item in value:
-                key_pairs.append(str(item))
-        else:
-            key_pairs.extend([f"--{key}", str(value)])
+        if key in ["entry_point", "project_name"]: continue
+        cmd.append(f"{key}={value}")
+    # for key, value in cfg.items():
+    #     if isinstance(value, bool):
+    #         key_list = key.split(".")
+    #         key_list[-1] = key_list[-1] if value else f"no-{key_list[-1]}"
+    #         key = ".".join(key_list)
+    #         key_pairs.append(f"--{key}")
+    #     elif isinstance(value, list):
+    #         key_pairs.append(f"--{key}")
+    #         for item in value:
+    #             key_pairs.append(str(item))
+    #     else:
+    #         key_pairs.extend([f"{key}=", str(value)])
 
-    cmd = ["python", "-m", entry_point] + key_pairs
+    
     print("Running command:", cmd)
     subprocess.run(cmd)
